23e65363fb1b7aa21bc3c750f24f5258c95f860f
snhampossa <73557357+snhampossa@users.noreply.github.com>
2020-10-28 18:59:01 +0530

    First website upload

---
+++ app.js
@@ -0,0 +1,63 @@
+
+  function hamburguer() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu-list") {
+      x.className += " responsive";
+    } else {
+      x.className = "menu-list";
+    }
+  }
+
+  function toggle() {
+    var x = document.getElementById("text");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+    function toggle1() {
+      var x = document.getElementById("text1");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+
+      function toggle2() {
+        var x = document.getElementById("text2");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+
+        function toggle3() {
+          var x = document.getElementById("text3");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+        }
+
+          function toggle4() {
+            var x = document.getElementById("text4");
+            if (x.style.display === "block") {
+              x.style.display = "none";
+            } else {
+              x.style.display = "block";
+            }
+          }
+
+            function toggle5() {
+              var x = document.getElementById("text5");
+              if (x.style.display === "block") {
+                x.style.display = "none";
+              } else {
+                x.style.display = "block";
+              }
+            }
